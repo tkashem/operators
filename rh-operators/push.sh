0cbfda0893af134c0422eb87c708edbb04ef0aa9
Lance Galletti <notgalletti>
2018-11-14 21:19:21 -0500

    Updating: update release

--- rh-operators/push.sh
+++ rh-operators/push.sh
@@ -3,7 +3,7 @@
 FILENAME="rh-operators"
 NAMESPACE="operators"
 REPOSITORY="rh-operators"
-RELEASE="1.10.0"
+RELEASE="1.11.0"
 
 # Remove leading and trailing double quotes, if there are any.
 TOKEN="${TOKEN%\"}"
